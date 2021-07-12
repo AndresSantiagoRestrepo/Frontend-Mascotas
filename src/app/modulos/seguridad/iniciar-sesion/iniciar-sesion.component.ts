a504e61ea0514c12b0262682dcba04f68f4b42cf
Stokker <stokkerma@gmail.com>
2021-07-12 14:03:52 -0500

    Creación de componentes inicio y cierre de sesion

--- src/app/modulos/seguridad/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/modulos/seguridad/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioModelo } from 'src/app/modelos/Usuario.modelo';
+import * as crypto from 'crypto-js'
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -10,7 +16,9 @@ export class IniciarSesionComponent implements OnInit {
 
   fgValidacion: FormGroup = this.fb.group({});
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    private servicioSeguridad: SeguridadService,
+    private router :Router) { }
 
   construirFormulario() {
 
@@ -28,6 +36,25 @@ export class IniciarSesionComponent implements OnInit {
 
   get obtenerFGV() {
     return this.fgValidacion.controls;
+  }
+
+  IdentificarUsuario() {
+    console.log("hola")
+    let usuario = this.obtenerFGV.correo.value;
+    let clave = this.obtenerFGV.clave.value;
+    let modelo = new UsuarioModelo();
+    modelo.nombre_usuario = usuario;
+    modelo.clave = crypto.MD5(clave).toString();
+    this.servicioSeguridad.IdentificarUsuario(modelo).subscribe(
+      (data: UsuarioModelo) => {
+        this.servicioSeguridad.GuardarDatosEnLocal(data)
+        this.router.navigate(["/"]);
+
+      },
+       (error: any) => {
+        alert("Datos invalidos")
+      })
+
   }
 
 }
